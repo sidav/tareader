7b8b637da49d3bd3e4a01b982ee2304a7ac79703
Vladislav Kovun <sidav94@gmail.com>
2024-01-20 19:54:42 +0100

    Remove zbuffer "flip". Optimize zbuffer clear-up.

--- raylib_renderer/renderer.go
+++ raylib_renderer/renderer.go
@@ -2,7 +2,6 @@ package raylibrenderer
 
 import (
 	"fmt"
-	"math"
 	"sort"
 	"time"
 	"totala_reader/model"
@@ -19,8 +18,8 @@ type RaylibRenderer struct {
 
 	frame int
 
-	zBuffer        [1920][1080]float64
-	zBufferReverse bool
+	zBuffer                                [1920][1080]float64
+	zBufMinX, zBufMaxX, zBufMinY, zBufMaxY int32 // for clearing changed area of the buffer only
 
 	trianglesBatch []*triangle
 }
@@ -32,17 +31,13 @@ func (r *RaylibRenderer) Init() {
 	r.fontSize = 32
 	r.scaleFactor = 5.0
 	middleware.Clear()
+	r.initZBuffer()
 	// middleware.Flush()
 }
 
 func (r *RaylibRenderer) DrawModel(rootObject *model.Model) {
 
-	for i := range r.zBuffer {
-		for j := range r.zBuffer[i] {
-			r.zBuffer[i][j] = -math.MaxFloat64
-		}
-	}
-	// r.flipZBuffer()
+	r.clearZBuffer()
 
 	r.trianglesBatch = nil
 	r.totalMessages = 0
--- raylib_renderer/zbuffer.go
+++ raylib_renderer/zbuffer.go
@@ -1,19 +1,43 @@
 package raylibrenderer
 
+import "math"
+
 func (r *RaylibRenderer) canDrawOverZBufferAt(x, y int32, depth float64) bool {
-	if r.zBufferReverse {
-		return -r.zBuffer[x][y] < depth
-	}
 	return r.zBuffer[x][y] < depth
 }
 
 func (r *RaylibRenderer) setZBufferValueAt(val float64, x, y int32) {
-	if r.zBufferReverse {
-		val = -val
-	}
 	r.zBuffer[x][y] = val
+	if x < r.zBufMinX {
+		r.zBufMinX = x
+	}
+	if x > r.zBufMaxX {
+		r.zBufMaxX = x
+	}
+	if y < r.zBufMinY {
+		r.zBufMinY = y
+	}
+	if y > r.zBufMaxY {
+		r.zBufMaxY = y
+	}
 }
 
-func (r *RaylibRenderer) flipZBuffer() {
-	r.zBufferReverse = !r.zBufferReverse
+func (r *RaylibRenderer) initZBuffer() {
+	r.zBufMinX, r.zBufMaxX = int32(len(r.zBuffer)), 0
+	r.zBufMinY, r.zBufMaxY = int32(len(r.zBuffer[0])), 0
+	for i := range r.zBuffer {
+		for j := range r.zBuffer[i] {
+			r.zBuffer[i][j] = -math.MaxFloat64
+		}
+	}
+}
+
+func (r *RaylibRenderer) clearZBuffer() {
+	for i := r.zBufMinX; i <= r.zBufMaxX; i++ {
+		for j := r.zBufMinY; j <= r.zBufMaxY; j++ {
+			r.zBuffer[i][j] = -math.MaxFloat64
+		}
+	}
+	r.zBufMinX, r.zBufMaxX = int32(len(r.zBuffer)), 0
+	r.zBufMinY, r.zBufMaxY = int32(len(r.zBuffer[0])), 0
 }
