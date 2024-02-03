f9986c4415100eb73ccb8ba3e9e68d1eb616ad3b
Vladislav Kovun <sidav94@gmail.com>
2024-02-04 00:47:58 +0100

    Refactor the matrix package.

--- geometry/matrix4x4/matrix4x4.go
+++ geometry/matrix4x4/matrix4x4.go
@@ -1,180 +1,25 @@
 package matrix4x4
 
-import (
-	"math"
-	"totala_reader/geometry"
-)
-
 type Matrix4x4 struct {
 	Vals [4][4]float64
 }
 
-func (m *Matrix4x4) InitAsUnitMatrix() {
+func NewUnitMatrix() *Matrix4x4 {
+	return &Matrix4x4{
+		Vals: [4][4]float64{
+			{1, 0, 0, 0},
+			{0, 1, 0, 0},
+			{0, 0, 1, 0},
+			{0, 0, 0, 1},
+		},
+	}
+}
+
+func (m *Matrix4x4) ResetToUnitMatrix() {
 	m.Vals = [4][4]float64{
 		{1, 0, 0, 0},
 		{0, 1, 0, 0},
 		{0, 0, 1, 0},
 		{0, 0, 0, 1},
-	}
-}
-
-// TODO: maybe use pointers?..
-func (m *Matrix4x4) MultiplyByVector(v geometry.Vector3) geometry.Vector3 {
-	// Matrix is 4x4, and the vector is 1x3. So, we consider the (1,4)th value as 1. THIS MAY BE A MISTAKE.
-	return geometry.Vector3{
-		X: m.Vals[0][0]*v.X + m.Vals[0][1]*v.Y + m.Vals[0][2]*v.Z + m.Vals[0][3],
-		Y: m.Vals[1][0]*v.X + m.Vals[1][1]*v.Y + m.Vals[1][2]*v.Z + m.Vals[1][3],
-		Z: m.Vals[2][0]*v.X + m.Vals[2][1]*v.Y + m.Vals[2][2]*v.Z + m.Vals[2][3],
-		// the next value is simply unused
-	}
-}
-
-func (m1 *Matrix4x4) MultiplyByMatrix4x4(m2 *Matrix4x4) *Matrix4x4 {
-	// matrix[line_number][column_number]
-	return &Matrix4x4{
-		Vals: [4][4]float64{
-			// row 1
-			{
-				m1.Vals[0][0]*m2.Vals[0][0] + m1.Vals[0][1]*m2.Vals[1][0] + m1.Vals[0][2]*m2.Vals[2][0] + m1.Vals[0][3]*m2.Vals[3][0],
-				m1.Vals[0][0]*m2.Vals[0][1] + m1.Vals[0][1]*m2.Vals[1][1] + m1.Vals[0][2]*m2.Vals[2][1] + m1.Vals[0][3]*m2.Vals[3][1],
-				m1.Vals[0][0]*m2.Vals[0][2] + m1.Vals[0][1]*m2.Vals[1][2] + m1.Vals[0][2]*m2.Vals[2][2] + m1.Vals[0][3]*m2.Vals[3][2],
-				m1.Vals[0][0]*m2.Vals[0][3] + m1.Vals[0][1]*m2.Vals[1][3] + m1.Vals[0][2]*m2.Vals[2][3] + m1.Vals[0][3]*m2.Vals[3][3],
-			},
-			// row 2
-			{
-				m1.Vals[1][0]*m2.Vals[0][0] + m1.Vals[1][1]*m2.Vals[1][0] + m1.Vals[1][2]*m2.Vals[2][0] + m1.Vals[1][3]*m2.Vals[3][0],
-				m1.Vals[1][0]*m2.Vals[0][1] + m1.Vals[1][1]*m2.Vals[1][1] + m1.Vals[1][2]*m2.Vals[2][1] + m1.Vals[1][3]*m2.Vals[3][1],
-				m1.Vals[1][0]*m2.Vals[0][2] + m1.Vals[1][1]*m2.Vals[1][2] + m1.Vals[1][2]*m2.Vals[2][2] + m1.Vals[1][3]*m2.Vals[3][2],
-				m1.Vals[1][0]*m2.Vals[0][3] + m1.Vals[1][1]*m2.Vals[1][3] + m1.Vals[1][2]*m2.Vals[2][3] + m1.Vals[1][3]*m2.Vals[3][3],
-			},
-			// row 3
-			{
-				m1.Vals[2][0]*m2.Vals[0][0] + m1.Vals[2][1]*m2.Vals[1][0] + m1.Vals[2][2]*m2.Vals[2][0] + m1.Vals[2][3]*m2.Vals[3][0],
-				m1.Vals[2][0]*m2.Vals[0][1] + m1.Vals[2][1]*m2.Vals[1][1] + m1.Vals[2][2]*m2.Vals[2][1] + m1.Vals[2][3]*m2.Vals[3][1],
-				m1.Vals[2][0]*m2.Vals[0][2] + m1.Vals[2][1]*m2.Vals[1][2] + m1.Vals[2][2]*m2.Vals[2][2] + m1.Vals[2][3]*m2.Vals[3][2],
-				m1.Vals[2][0]*m2.Vals[0][3] + m1.Vals[2][1]*m2.Vals[1][3] + m1.Vals[2][2]*m2.Vals[2][3] + m1.Vals[2][3]*m2.Vals[3][3],
-			},
-			// row 4
-			{
-				m1.Vals[3][0]*m2.Vals[0][0] + m1.Vals[3][1]*m2.Vals[1][0] + m1.Vals[3][2]*m2.Vals[2][0] + m1.Vals[3][3]*m2.Vals[3][0],
-				m1.Vals[3][0]*m2.Vals[0][1] + m1.Vals[3][1]*m2.Vals[1][1] + m1.Vals[3][2]*m2.Vals[2][1] + m1.Vals[3][3]*m2.Vals[3][1],
-				m1.Vals[3][0]*m2.Vals[0][2] + m1.Vals[3][1]*m2.Vals[1][2] + m1.Vals[3][2]*m2.Vals[2][2] + m1.Vals[3][3]*m2.Vals[3][2],
-				m1.Vals[3][0]*m2.Vals[0][3] + m1.Vals[3][1]*m2.Vals[1][3] + m1.Vals[3][2]*m2.Vals[2][3] + m1.Vals[3][3]*m2.Vals[3][3],
-			},
-		},
-	}
-}
-
-func (m1 *Matrix4x4) GetRotatedAroundX(radians float64) *Matrix4x4 {
-	// TODO: rewrite without returning value; change the m1 in place
-	sin := math.Sin(radians)
-	cos := math.Cos(radians)
-	return &Matrix4x4{
-		Vals: [4][4]float64{
-			// row 1
-			{
-				m1.Vals[0][0],
-				m1.Vals[0][1]*cos + m1.Vals[0][2]*(-sin),
-				m1.Vals[0][1]*sin + m1.Vals[0][2]*cos,
-				m1.Vals[0][3],
-			},
-			// row 2
-			{
-				m1.Vals[1][0],
-				m1.Vals[1][1]*cos + m1.Vals[1][2]*(-sin),
-				m1.Vals[1][1]*sin + m1.Vals[1][2]*cos,
-				m1.Vals[1][3],
-			},
-			// row 3
-			{
-				m1.Vals[2][0],
-				m1.Vals[2][1]*cos + m1.Vals[2][2]*(-sin),
-				m1.Vals[2][1]*sin + m1.Vals[2][2]*cos,
-				m1.Vals[2][3],
-			},
-			// row 4
-			{
-				m1.Vals[3][0],
-				m1.Vals[3][1]*cos + m1.Vals[3][2]*(-sin),
-				m1.Vals[3][1]*sin + m1.Vals[3][2]*cos,
-				m1.Vals[3][3],
-			},
-		},
-	}
-}
-
-func (m1 *Matrix4x4) GetRotatedAroundY(radians float64) *Matrix4x4 {
-	sin := math.Sin(radians)
-	cos := math.Cos(radians)
-	// matrix[line_number][column_number]
-	return &Matrix4x4{
-		Vals: [4][4]float64{
-			// row 1
-			{
-				m1.Vals[0][0]*cos + m1.Vals[0][2]*sin,
-				m1.Vals[0][1],
-				m1.Vals[0][0]*(-sin) + m1.Vals[0][2]*cos,
-				m1.Vals[0][3],
-			},
-			// row 2
-			{
-				m1.Vals[1][0]*cos + m1.Vals[1][2]*sin,
-				m1.Vals[1][1],
-				m1.Vals[1][0]*(-sin) + m1.Vals[1][2]*cos,
-				m1.Vals[1][3],
-			},
-			// row 3
-			{
-				m1.Vals[2][0]*cos + m1.Vals[2][2]*sin,
-				m1.Vals[2][1],
-				m1.Vals[2][0]*(-sin) + m1.Vals[2][2]*cos,
-				m1.Vals[2][3],
-			},
-			// row 4
-			{
-				m1.Vals[3][0]*cos + m1.Vals[3][2]*sin,
-				m1.Vals[3][1],
-				m1.Vals[3][0]*(-sin) + m1.Vals[3][2]*cos,
-				m1.Vals[3][3],
-			},
-		},
-	}
-}
-
-func (m1 *Matrix4x4) GetRotatedAroundZ(radians float64) *Matrix4x4 {
-	sin := math.Sin(radians)
-	cos := math.Cos(radians)
-	// matrix[line_number][column_number]
-	return &Matrix4x4{
-		Vals: [4][4]float64{
-			// row 1
-			{
-				m1.Vals[0][0]*cos + m1.Vals[0][1]*sin,
-				m1.Vals[0][0]*(-sin) + m1.Vals[0][1]*cos,
-				m1.Vals[0][2],
-				m1.Vals[0][3],
-			},
-			// row 2
-			{
-				m1.Vals[1][0]*cos + m1.Vals[1][1]*sin,
-				m1.Vals[1][0]*(-sin) + m1.Vals[1][1]*cos,
-				m1.Vals[1][2],
-				m1.Vals[1][3],
-			},
-			// row 3
-			{
-				m1.Vals[2][0]*cos + m1.Vals[2][1]*sin,
-				m1.Vals[2][0]*(-sin) + m1.Vals[2][1]*cos,
-				m1.Vals[2][2],
-				m1.Vals[2][3],
-			},
-			// row 4
-			{
-				m1.Vals[3][0]*cos + m1.Vals[3][1]*sin,
-				m1.Vals[3][0]*(-sin) + m1.Vals[3][1]*cos,
-				m1.Vals[3][2],
-				m1.Vals[3][3],
-			},
-		},
 	}
 }
